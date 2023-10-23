ea9e994ede4ec9b59da976b2dc7c5657e0974f7a
KunalNarkhedePatil <narkhedekunal53@gmail.com>
2023-10-23 10:48:52 +0530

    commit

---
+++ static.cs
@@ -0,0 +1,44 @@
+using System;
+
+
+class Demo
+{
+    public int X;
+    public static int Y;
+    
+    public Demo()
+    {
+         Console.WriteLine("Inside default constructor");
+        this.X=10;
+    }
+    static Demo()
+    {
+        Console.WriteLine("Inside static constructor");
+        Y=20;
+    }
+    
+    public void fun()
+    {
+        Console.WriteLine("Inside non static fun..");
+        Console.WriteLine("Value of X:"+this.X);
+        Console.WriteLine("Value of Y:"+Demo.Y);
+    }
+    public static void gun()
+    {
+        Console.WriteLine("Inside static gun..");
+        //Console.WriteLine("Value of X:"+this.X);
+        Console.WriteLine("Value of Y:"+Demo.Y);
+    }
+}
+class StaticDemo
+{
+    public static void Main(String[] args)
+    {
+        Demo obj=new Demo();
+        obj.fun();
+        Demo.gun();
+        
+        Console.WriteLine("Value of X:"+obj.X);
+        Console.WriteLine("Value of Y:"+Demo.Y);
+    }
+}
